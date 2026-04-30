0dc746887722b72f6074c2172dab195893b22761
RodolfoFigueroa <4rodolfofigueroa2@gmail.com>
2026-04-30 13:46:54 -0600

    Add segregation assets

--- census_processing/defs/assets/income/income.py
+++ census_processing/defs/assets/income/income.py
@@ -41,10 +41,11 @@ def calculate_local_gini(df_cdf: pd.DataFrame) -> pd.Series:
 
 @dg.op
 def calculate_income(
+    context: dg.OpExecutionContext,
     df_survey: pd.DataFrame,
     df_census: pd.DataFrame,
     df_census_geometry: gpd.GeoDataFrame,
-) -> pd.DataFrame:
+) -> dict:
     seed_xr = ipf.generate_contingency_table(df_survey, INCOME_LINKING_COLS)
     ds = ipf.apply_ipf(df_census, seed_xr)
     df_ind = ipf.generate_individual_weights(df_survey, ds)
@@ -53,49 +54,90 @@ def calculate_income(
         ["cvegeo", "income", "income_pc"]
     ]
 
-    _, df_cdf, _, _, _ = seg.global_H_index(df_ind, df_census.index.to_list())
-    return (
+    h, df_cdf, _, _, _ = seg.global_H_index(df_ind, df_census.index.to_list())
+    out_df = (
         pop_income.set_index("cvegeo")
         .assign(gini=calculate_local_gini(df_cdf.reset_index()))
         .reset_index()
     )
+    return {"H": h, "income": out_df, "mapping_key": context.get_mapping_key()}
 
 
 @dg.graph
-def full_graph(met_zone: str, agebs_2020: None) -> pd.DataFrame:
+def full_graph(met_zone: str, agebs_2020: None) -> dict:
     df_census = load_census(met_zone, agebs_2020)
     df_census_geometries = load_census_geometries(met_zone, agebs_2020)
     df_survey = load_survey(met_zone)
     return calculate_income(df_survey, df_census, df_census_geometries)
 
 
-@dg.op(out=dg.Out(io_manager_key="dataframe_postgres_manager"))
-def concat_results(df_list: list[pd.DataFrame]) -> pd.DataFrame:
-    return pd.concat(df_list, ignore_index=True)
+@dg.op(
+    out={
+        "income": dg.Out(io_manager_key="dataframe_postgres_manager"),
+        "H": dg.Out(io_manager_key="dataframe_postgres_manager"),
+    }
+)
+def concat_results(results_list: list[dict]) -> tuple[pd.DataFrame, pd.DataFrame]:
+    df_list, h_map = [], {}
+    for submap in results_list:
+        df_list.append(submap["income"])
+        h_map[submap["mapping_key"]] = submap["H"]
+
+    df_income = pd.concat(df_list, ignore_index=True)
+    df_h = (
+        pd.Series(h_map, name="H")
+        .to_frame()
+        .reset_index(names="cve_met")
+        .assign(cve_met=lambda df: df["cve_met"].str.replace("_", "."))
+    )
+    print(df_h)
+    return df_income, df_h
 
 
-@dg.graph_asset(
-    key="income",
+@dg.graph_multi_asset(
     ins={
         "agebs_2020": dg.AssetIn(
             key=["census", "2020", "ageb"], dagster_type=dg.Nothing
         )
     },
-    group_name="income",
-    metadata={
-        "primary_key": "cvegeo",
-        "table_name": "income_2020",
-        "foreign_keys": [
-            {
-                "column": "cvegeo",
-                "ref_table": "census_2020_ageb",
-                "ref_column": "cvegeo",
-            }
-        ],
+    outs={
+        "income": dg.AssetOut(
+            key="income",
+            metadata={
+                "primary_key": "cvegeo",
+                "table_name": "income_2020",
+                "foreign_keys": [
+                    {
+                        "column": "cvegeo",
+                        "ref_table": "census_2020_ageb",
+                        "ref_column": "cvegeo",
+                    }
+                ],
+            },
+        ),
+        "H": dg.AssetOut(
+            key="H",
+            metadata={
+                "primary_key": "cve_met",
+                "table_name": "segregation_met_zone",
+                "foreign_keys": [
+                    {
+                        "column": "cve_met",
+                        "ref_table": "metropoli_2020",
+                        "ref_column": "cve_met",
+                    }
+                ],
+            },
+        ),
     },
+    group_name="income",
 )
-def income(agebs_2020: None) -> pd.DataFrame:
+def income(agebs_2020: None) -> dict[str, pd.DataFrame]:
     met_zones = get_met_zones()
-    return concat_results(
+    df_income, df_h = concat_results(
         met_zones.map(lambda met_zone: full_graph(met_zone, agebs_2020)).collect()
     )
+    return {
+        "income": df_income,
+        "H": df_h,
+    }
